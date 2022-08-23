2eb4987caea0d00ef09bc3b2d9f88791394de299
410 <me@hysong.dev>
2022-08-24 00:41:00 +0900

    Initial commit

---
+++ src/me/libhycore/CoreLogger.java
@@ -0,0 +1,30 @@
+package me.libhycore;
+
+public class CoreLogger {
+
+    private static boolean printDebug = false;
+
+    public static enum EventType {
+        INFO, WARNING, ERROR
+    }
+
+    public static void print(EventType eventType, String message) {
+        StackTraceElement ste = Thread.currentThread().getStackTrace()[2];
+        String callerClass = ste.getClassName();
+        String time = CoreDate.timestamp();
+        String s = "[" + time + "] [" + callerClass + "] [" + eventType.toString() + "] " + message;
+        if (eventType.equals(EventType.ERROR)) {
+            System.err.println(s);
+        }else{
+            System.out.println(s);    
+        }
+    }
+
+    public static void debug(EventType eventType, String message) {
+        if (printDebug) print(eventType, message);
+    }
+
+    public static void setDebugPrinting(boolean printDebug) {
+        CoreLogger.printDebug = printDebug;
+    }
+}
---
+++ src/me/libhycore/exceptions/InvalidInputException.java
@@ -0,0 +1,7 @@
+package me.libhycore.exceptions;
+
+public class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
